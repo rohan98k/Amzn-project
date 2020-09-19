7440cf565e209ff3d9c083356146b4bbeda0d4bd
Rohan Kamath <rohankamath1998@yahoo.com>
2020-09-19 16:02:46 +0530

    init

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/amzn-rohan98k/us-central1/api",
+  baseURL: "https://us-central1-amzn-rohan98k.cloudfunctions.net/api",
+  //http://localhost:5001/amzn-rohan98k/us-central1/api
 });
 
 export default instance;
